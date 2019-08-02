083b4fcee7b8a62f25704e6523f70658c5c741f5
HXB <xiaobinbingor@163.com>
2019-08-02 17:51:37 +0800

    1

--- BingorRouter/router/src/main/java/com/bingor/router/Keys.java
+++ BingorRouter/router/src/main/java/com/bingor/router/Keys.java
@@ -4,5 +4,4 @@ package com.bingor.router;
  * Created by Bingor on 2019/7/27.
  */
 public class Keys {
-   public static final String KEY_SERVICE_COM_BINGOR_TESTLIB_TESTSERVICE = "COM_BINGOR_TESTLIB_TESTSERVICE";
 }
